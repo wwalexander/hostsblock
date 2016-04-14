b1ecd8275e15e6f747da64fe8ad624c96c63d87d
Will Alexander <wwalexander@users.noreply.github.com>
2016-04-13 21:08:53 -0700

    Removing trailing space from hosts line

--- hostsblock.c
+++ hostsblock.c
@@ -132,7 +132,7 @@ int main(int argc, char **argv)
 		if (feof(list) || ferror(list)) break;
 
 		if (!get_domain(rule, domain)) {
-			printf("0 \t%s\n", domain);
+			printf("0\t%s\n", domain);
 		}
 	}
 
